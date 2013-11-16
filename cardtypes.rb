61b6b13da0f84c7305547387e4ebde6a20c6a5b6
Brenda Streiff <git@streiff.net>
2013-11-16 09:45:18 -0600

    These are gone now.

--- cardtypes.rb
+++
@@ -1,8 +0,0 @@
-# Trends Against Humanity
-#
-# See LICENSE for license details.
-
-module TrendsAgainstHumanity
-   BlackCard = Struct.new(:text, :draw, :pick, :woeid)
-   WhiteCard = Struct.new(:text)
-end
